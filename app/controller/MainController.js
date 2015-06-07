4c9a95dd8e2b0a15fa3ddb31261c336e1668237a
Rams <ramarao.manthri@gmail.com>
2015-06-07 13:31:21 +0530

    Header part is done

--- app/controller/MainController.js
+++ app/controller/MainController.js
@@ -1,5 +1,15 @@
 define(['app'],function(app){
-  app.controller('MainController',function($scope){
+  app.controller('MainController',function($scope,$window){
     $scope.message = "Welcome!";
+
+  angular.element($window).bind('scroll',function(){
+    if(window.scrollY > 30){
+     $('.rr-header img.logo').css({'width':'100px'});
+    }else{
+     $('.rr-header img.logo').css({'width':''});
+    }
+    //console.log('scrolling');
+  });
+
   });
 });
